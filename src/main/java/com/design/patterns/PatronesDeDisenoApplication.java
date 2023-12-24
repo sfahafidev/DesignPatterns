aab7a1816ca8ec3fc2f84f27c6ecefbfd8aa6a20
sfahafidev <sfahafidev@gmail.com>
2023-12-24 01:08:11 -0300

    Primer commit

---
+++ src/main/java/com/design/patterns/PatronesDeDisenoApplication.java
@@ -0,0 +1,13 @@
+package com.design.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronesDeDisenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatronesDeDisenoApplication.class, args);
+	}
+
+}
